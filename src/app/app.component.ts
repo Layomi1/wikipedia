386ceca8c8fcdb3392d9e4a2f6c9a0646b5dcc07
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2023-05-18 16:15:22 +0100

    The full implemetation. 	modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts   modified:   src/app/app.module.ts 	new file:   src/app/wiki.service.spec.ts 	new file:   src/app/wiki.service.ts 	modified:   src/index.html 	new file:   search-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'wikipedia';
+  searchTerm: any;
+  results: any = [];
+
+  totalResults: any;
+  page: number = 1;
+
+  constructor(private service: WikiService) {}
+
+  onSubmit() {
+    this.service.search(this.searchTerm).subscribe((res: any) => {
+      this.results = res.query.search;
+      // pagination
+      this.totalResults = res.query.search.length;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, NgxPaginationModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
